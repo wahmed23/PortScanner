8e2a4e965837ac19e99a1acc82e2135199234f7a
root <root@AirporGroottOfficial>
2018-10-29 05:54:53 -0400

    Options parser

--- PortScanner/src/PortScanner/PortScanner.py
+++ PortScanner/src/PortScanner/PortScanner.py
@@ -4,8 +4,17 @@ Created on Oct 28, 2018
 @author: groot
 '''
 
-def main():
-    print('YOLO')
+import optparse
 
+def main():
+    parser = optparse.OptionParser('usage %prog -H <target_host> -p <target_ip>')
+    parser.add_option('-H', dest='tgt_host', type='string', help='specify target host')
+    parser.add_option('-p', dest='tgt_port', type='string', help='specify target port')
+    (options, args) = parser.parse_args()
+    tgt_host = options.tgt_host
+    tgt_port = options.tgt_port
+    if (tgt_host == None) | (tgt_port == None):
+        print parser.usage
+        exit(0)
 if __name__ == '__main__':
     main()
