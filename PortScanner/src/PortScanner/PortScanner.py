6f2605a0f7c4833d8728cde18a275e4b18a9a27c
root <grootroot@AirportOfficial>
2018-10-30 04:54:57 -0400

    Add custom error message compatible with Python and python3

--- PortScanner/src/PortScanner/PortScanner.py
+++ PortScanner/src/PortScanner/PortScanner.py
@@ -23,29 +23,29 @@ def portScan(hostname, targetPorts):
 
 def connScan(tgt_host, tgt_port):
     try:
+        tgt_port_int = int(tgt_port)
         connSkt = socket(AF_INET, SOCK_STREAM)
-        connSkt.connect((tgt_host, tgt_port))
+        connSkt.connect((tgt_host, tgt_port_int))
         connSkt.sendall(b'Violent Python')
         results = connSkt.recv(100)
-        print ('[+] %d/tcp open'% tgt_port)
-        print ('[+] '+str(results))
+        print ('[+] %d/tcp open'% tgt_port_int)
+        print ('[+] '+results.decode('utf-8'))
         connSkt.close()
     except:
-        print('[-] %d/tcp closed'% tgt_port)
+        print('[-] %d/tcp closed'% tgt_port_int)
 
 def main():
-    parser = optparse.OptionParser('usage %prog -H <target_host> -p <target_port>')
+    parser = optparse.OptionParser('usage%prog -H <target_host> -p <target_port>')
     parser.add_option('-H', dest='tgt_host', type='string', help='specify target host')
-    parser.add_option('-p', dest='tgt_port', type='string', help='specify target port')
+    parser.add_option('-p', dest='tgt_port', type='string', help='specify target port[s] seperated by comma')
     (options, args) = parser.parse_args()
     tgt_host = options.tgt_host
-    tgt_port = options.tgt_port
-    if (tgt_host == None) | (tgt_port == None):
-        print (parser.usage)
+    tgt_port = str(options.tgt_port).split(',')
+    if (tgt_host == None) | (tgt_port[0] == None):
+        print('[-] You must specify a target host and port[s]')
         exit(0)
     else:
-        portList = [21,22,25,80,443]
-        portScan(tgt_host, portList)
+        portScan(tgt_host, tgt_port)
 
 if __name__ == '__main__':
     main()
